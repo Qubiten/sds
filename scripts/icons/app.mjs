d52fe1931447cab27b94bc05b4f371437d69819e
wilbye <wmmbabu@Mac.lan>
2026-02-28 08:40:33 +0300

    Remove props: size: figma.enum

--- scripts/icons/app.mjs
+++ scripts/icons/app.mjs
@@ -222,7 +222,7 @@ async function fileRESTResponseToIconComponentsJSON(response) {
     svgString.push(");");
     // Code Connect doc code
     figmaString.push(
-      `figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${componentSetId}", { props: { size: figma.enum("Size", { "20": "20", "24": "24", "32": "32", "40": "40", "48": "48" }) }, example: ({ size }) => <${name} size={size} /> });`,
+      `figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${componentSetId}", { example: () => <${name} /> });`,
     );
     // Add the strings for this component into our result.
     result.push([name, svgString.join("\n"), figmaString.join("\n")]);
